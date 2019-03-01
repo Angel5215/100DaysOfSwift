1e1b9db8c779454d6a38cef63deccf15471a4c9e
Ángel Vázquez <angel_betta@outlook.com>
2019-02-28 22:53:14 -0600

    Added day 27

--- Project05/Project05/ViewController.swift
+++ Project05/Project05/ViewController.swift
@@ -16,12 +16,15 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
             }
         }
         
+        //  isEmpty is more efficient than count == 0 for certain collections that iterate over all elements to check if the count is zero.
         if allWords.isEmpty {
             allWords = ["silkworm"]
         }
@@ -43,6 +46,23 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        
     }
 }
 
