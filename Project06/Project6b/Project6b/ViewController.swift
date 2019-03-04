b77d5e1d623dc4ea9293fd8e62ee0bd17ac93247
Ángel Vázquez <angel_betta@outlook.com>
2019-03-04 13:30:23 -0600

    Added day 31

--- Project06/Project6b/Project6b/ViewController.swift
+++ Project06/Project6b/Project6b/ViewController.swift
@@ -49,6 +49,7 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
+        /*
         //  Auto Layout using Visual Format Language (VFL)
         let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
         
@@ -57,10 +58,36 @@ class ViewController: UIViewController {
             view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
         
+        
+        //  Use metrics to give VFL a set of sized with names instead of using hard-coded numbers.
+        let metrics = ["labelHeight": 88]
+        
         //  - means space of 10 points by default.
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+        */
         
-        
+        //  Using anchors
+        var previous: UILabel?
+        for label in [label1, label2, label3, label4, label5] {
+            
+            //label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            
+            //  Challenges 1 & 2. Using the leading and trailing anchor to explicitly pin the label to the edges. Using the safeAreaLayoutGuide so taht the labels won't go under the safe area.
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            
+            //  Challenge 3. Making the labels' height equal to 1/5 of the screen minus 10 for the spacing.
+            label.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+            
+            
+            //label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            }
+            
+            previous = label
+        }
     }
 }
 
