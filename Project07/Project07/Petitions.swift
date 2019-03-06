46ef899976447104be3087deb2f7534f47de3398
Ángel Vázquez <angel_betta@outlook.com>
2019-03-06 02:18:47 -0600

    Added Project 07 - part 1. Day 33

---
+++ Project07/Project07/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project07
+//
+//  Created by Angel Vázquez on 3/6/19.
+//  Copyright © 2019 Angel Vázquez. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
