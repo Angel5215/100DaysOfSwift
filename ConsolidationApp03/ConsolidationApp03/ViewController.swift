f0e81922b5bfe68be747f296b2738e4b17a0c23b
Ángel Vázquez <angel_betta@outlook.com>
2019-03-13 18:08:28 -0600

    Added Challenge project from day 41

---
+++ ConsolidationApp03/ConsolidationApp03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConsolidationApp03
+//
+//  Created by Angel Vázquez on 3/13/19.
+//  Copyright © 2019 Angel Vázquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
