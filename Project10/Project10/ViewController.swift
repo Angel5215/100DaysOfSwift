8d9053d4838d31feb4c181eb3cc9084fb64bcb11
Ángel Vázquez <angel_betta@outlook.com>
2019-03-15 22:20:25 -0600

    Added Day 42. Collection View

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Angel Vázquez on 3/14/19.
+//  Copyright © 2019 Angel Vázquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath)
+        
+        return cell
+    }
+
+}
+
