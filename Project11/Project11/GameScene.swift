f88edf5b7924db4a6d907155a1ea8491bd865862
Ángel Vázquez <angel_betta@outlook.com>
2019-03-17 23:24:14 -0600

    Day 45. Changed boxes to red balls with circle physics.

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -16,16 +16,31 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         
+        for i in 0 ..< 5 {
+            makeBounceer(at: CGPoint(x: 256 * i, y: 0))
+        }
+        
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let location = touch.location(in: self)
-            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
-            box.position = location
-            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-            addChild(box)
+            
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
+            ball.physicsBody?.restitution = 0.4
+            ball.position = location
+            
+            addChild(ball)
         }
+    }
+    
+    func makeBounceer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
     }
 }
