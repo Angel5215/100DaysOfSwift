c6aa1f1682d6cea6468c49a2334446f5e6bf8cb0
Ángel Vázquez <angel_betta@outlook.com>
2019-03-21 16:30:22 -0600

    Added Day 48. UserDefaults and the Codable protocol

---
+++ Project12/Project12b/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Angel Vázquez on 3/14/19.
+//  Copyright © 2019 Angel Vázquez. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
