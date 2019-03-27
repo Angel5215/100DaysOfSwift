153775efd9260e540b7a2d05d541cc8ddcafa5ba
Ángel Vázquez <angel_betta@outlook.com>
2019-03-27 16:59:27 -0600

    Added challenges from day 54.

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -12,7 +12,17 @@ import CoreImage
 class ViewController: UIViewController {
 
     @IBOutlet var imageView: UIImageView!
+    
     @IBOutlet var intensity: UISlider!
+    
+    @IBOutlet var centerX: UISlider!
+    @IBOutlet var centerY: UISlider!
+    
+    
+    @IBOutlet var radius: UISlider!
+    @IBOutlet var scale: UISlider!
+    
+    @IBOutlet var changeFilterButton: UIButton!
     
     //  Handles rendering.
     var context: CIContext!
@@ -21,6 +31,7 @@ class ViewController: UIViewController {
     
     var currentImage: UIImage!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,7 +39,12 @@ class ViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
         
         context = CIContext()
-        currentFilter = CIFilter(name: "CISepiaTone")
+        
+        let initialFilterTitle = "CISepiaTone"
+        currentFilter = CIFilter(name: initialFilterTitle)
+        changeFilterButton.setTitle(initialFilterTitle, for: .normal)
+        setSliders()
+        
     }
     
     @objc func importPicture() {
@@ -41,14 +57,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func changeFilter(_ sender: Any) {
+        
+        let filterNames = ["CIBumpDistortion", "CIGaussianBlur", "CIPixellate", "CISepiaTone", "CITwirlDistortion", "CIUnsharpMask", "CIVignette", "CICircularWrap", "CIVortexDistortion"]
+        
         let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
-        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
-        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
-        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
-        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
-        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
-        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        
+        for name in filterNames {
+            ac.addAction(UIAlertAction(title: name, style: .default, handler: setFilter))
+        }
+        
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         present(ac, animated: true)
     }
@@ -58,15 +75,22 @@ class ViewController: UIViewController {
         guard let actionTitle = action.title else { return }
         
         currentFilter = CIFilter(name: actionTitle)
+        changeFilterButton.setTitle(actionTitle, for: .normal)
         
         let beginImage = CIImage(image: currentImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         
+        setSliders()
         applyProcessing()
     }
     
     @IBAction func save(_ sender: Any) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            let ac = UIAlertController(title: "Error", message: "There's no image to save.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(ac, animated: true)
+            return
+        }
         
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
@@ -88,19 +112,35 @@ class ViewController: UIViewController {
     }
     
     func applyProcessing() {
+        guard let image = currentFilter.outputImage else { return }
+        
         let inputKeys = currentFilter.inputKeys
         
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
-        if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
-        
-        guard let image = currentFilter.outputImage else { return }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radius.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(scale.value * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputCenterKey) {
+            
+            let x = currentImage.size.width * CGFloat(centerX.value)
+            let y = currentImage.size.width * CGFloat(centerY.value)
+            
+            currentFilter.setValue(CIVector(x: x, y: y), forKey: kCIInputCenterKey)
+            
+        }
         
         if let cgimg = context.createCGImage(image, from: image.extent) {
             let processedImage = UIImage(cgImage: cgimg)
             self.imageView.image = processedImage
         }
+    }
+    
+    func setSliders() {
+        let inputKeys = currentFilter.inputKeys
+        intensity.isEnabled = inputKeys.contains(kCIInputIntensityKey)
+        radius.isEnabled = inputKeys.contains(kCIInputRadiusKey)
+        scale.isEnabled = inputKeys.contains(kCIInputScaleKey)
+        centerX.isEnabled = inputKeys.contains(kCIInputCenterKey)
+        centerY.isEnabled = inputKeys.contains(kCIInputCenterKey)
     }
 }
 
