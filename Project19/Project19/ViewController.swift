d1c347bbd4a3da1f5cbb39af9f918b67720662b9
Ángel Vázquez <angel_betta@outlook.com>
2019-04-09 22:45:04 -0500

    Introduction to app extensions. Day 67.

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Angel Vázquez on 4/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
