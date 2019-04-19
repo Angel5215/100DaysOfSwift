883ee9893595f7200668f07b84430b4993fac52b
Ángel Vázquez <angel_betta@outlook.com>
2019-04-19 18:36:14 -0500

    Project 23. Initial commit.

---
+++ Project23/Project23/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by Angel Vázquez on 4/19/19.
+//  Copyright © 2019 Angel Vázquez. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+}
