82b4231a809faa288d1b4702bba421fe8cfc02c2
Ángel Vázquez <angel_betta@outlook.com>
2019-05-12 17:29:13 -0500

    chore: Set project 27 initial files and configuration
    Configuration includes being an iPad-only app and images to draw
    later using CoreGraphics.

---
+++ Project27/Project27/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project27
+//
+//  Created by Angel Vázquez on 5/12/19.
+//  Copyright © 2019 Angel Vázquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
